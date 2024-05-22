a9721002dfeef2ebf8493bf70d4ddcaf85711a2d
Connie Leung <cleung334@gmail.com>
2024-05-22 17:51:51 +0800

    refactor: delete inject functions

--- nestjs-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
+++ nestjs-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
@@ -1,42 +1,30 @@
 import { GenerativeModel } from '@google/generative-ai';
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import {
+  GEMINI_ADVISORY_FEEDBACK_MODEL,
+  GEMINI_FIND_LANGUAGE_MODEL,
+  GEMINI_SENTIMENT_ANALYSIS_MODEL,
+} from './constants/gemini.constant';
 import { SentimentAnalysis } from './types/sentiment-analysis.type';
-import { injectAnalysisModel, injectFeedbackModel, injectFindLanguageModel } from './injectors/models.injector';
 
 @Injectable()
 export class AdvisoryFeedbackPromptChainingService {
   private readonly logger = new Logger(AdvisoryFeedbackPromptChainingService.name);
 
   constructor(
-    @injectAnalysisModel() private analysisModel: GenerativeModel,
-    @injectFeedbackModel() private feedbackModel: GenerativeModel,
-    @injectFindLanguageModel() private findLanguageModel: GenerativeModel,
+    @Inject(GEMINI_SENTIMENT_ANALYSIS_MODEL) private analysisModel: GenerativeModel,
+    @Inject(GEMINI_ADVISORY_FEEDBACK_MODEL) private feedbackModel: GenerativeModel,
+    @Inject(GEMINI_FIND_LANGUAGE_MODEL) private findLanguageModel: GenerativeModel,
   ) {}
-
-  private async generateSentinment(prompt: string): Promise<SentimentAnalysis> {
-    try {
-      const result = await this.analysisModel.generateContent(prompt);
-      const response = await result.response;
-      const text = response.text();
-      this.logger.log(text);
-      return JSON.parse(text) as SentimentAnalysis;
-    } catch (ex) {
-      console.error(ex);
-      throw ex;
-    }
-  }
 
   async generateFeedback(prompt: string) {
     try {
-      const [{ sentiment, topic }, language] = await Promise.all([
-        this.generateSentinment(prompt),
-        this.findLanguage(prompt),
-      ]);
-
+      const [analysis, language] = await Promise.all([this.analyseSentinment(prompt), this.findLanguage(prompt)]);
+      const { sentiment, topic } = analysis;
       const chainedPrompt = `
         The customer wrote a ${sentiment} feedback about ${topic} in ${language}. Provided feedback: ${prompt}.
         Feedback: 
-        `;
+      `;
 
       this.logger.log(chainedPrompt);
       const result = await this.feedbackModel.generateContent(chainedPrompt);
@@ -50,10 +38,25 @@ export class AdvisoryFeedbackPromptChainingService {
     }
   }
 
+  private async analyseSentinment(prompt: string): Promise<SentimentAnalysis> {
+    try {
+      const result = await this.analysisModel.generateContent(prompt);
+      const response = await result.response;
+      return JSON.parse(response.text()) as SentimentAnalysis;
+    } catch (ex) {
+      console.error(ex);
+      throw ex;
+    }
+  }
+
   private async findLanguage(prompt: string): Promise<string> {
-    const languageResult = await this.findLanguageModel.generateContent(prompt);
-    const languageResponse = await languageResult.response;
-    const language = languageResponse.text();
-    return language;
+    try {
+      const languageResult = await this.findLanguageModel.generateContent(prompt);
+      const languageResponse = await languageResult.response;
+      return languageResponse.text();
+    } catch (ex) {
+      console.error(ex);
+      throw ex;
+    }
   }
 }
