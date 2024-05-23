c3e43bfe38ddbdf59c9dda2fbda6e81a5507fb6c
Connie Leung <cleung334@gmail.com>
2024-05-24 00:03:50 +0800

    feat: add advisoryFeedback services

--- nestjs-groq-customer-feedback/src/advisory-feedback/advisory-feedback.module.ts
+++ nestjs-groq-customer-feedback/src/advisory-feedback/advisory-feedback.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AdvisoryFeedbackPromptChainingService } from './application/advisory-feedback-prompt-chaining.service';
+import { AdvisoryFeedbackService } from './application/advisory-feedback.service';
 import { GroqProvider } from './application/providers/groq.provider';
 import { AdvisoryFeedbackController } from './presenters/https/advisory-feedback.controller';
 
 @Module({
   controllers: [AdvisoryFeedbackController],
-  providers: [GroqProvider],
+  providers: [GroqProvider, AdvisoryFeedbackPromptChainingService, AdvisoryFeedbackService],
 })
 export class AdvisoryFeedbackModule {}
