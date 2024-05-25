36a77d5729f97da4a18056cb6e05ed29a8b56357
Connie Leung <cleung334@gmail.com>
2024-05-25 21:04:29 +0800

    refactor: create topic and sentiment chains

--- nestjs-langchain-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
+++ nestjs-langchain-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
@@ -1,13 +1,11 @@
-import { JsonOutputParser, StringOutputParser } from '@langchain/core/output_parsers';
+import { StringOutputParser } from '@langchain/core/output_parsers';
 import { PromptTemplate } from '@langchain/core/prompts';
+import { RunnableSequence } from '@langchain/core/runnables';
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { GEMINI_CHAT_MODEL } from './constants/gemini.constant';
-import { RunnableSequence } from '@langchain/core/runnables';
 
-type RunnableFeedbackInput = {
-  feedback: string;
-};
+// https://www.kaggle.com/code/marcinrutecki/langchain-multiple-chains-simply-explained
 
 @Injectable()
 export class AdvisoryFeedbackPromptChainingService {
@@ -17,8 +15,9 @@ export class AdvisoryFeedbackPromptChainingService {
 
   // three chains:
   // chain 1: find the language of the feedback
-  // chain 2: identity the sentiment and topic of the feedback
-  // chain 3: combine chain 1 and 2 into a new chain. Then, address the topic and reply in the same language
+  // chain 2: identity the sentiment of the feedback
+  // chain 3: identity the topic of the feedback
+  // chain 4: combine chain 1, 2 and 3 into a new chain. Then, address the topic and reply in the same language
   private createFindLanguageChain() {
     const languageTemplate = `What is the language of this feedback? 
     If the feedback is written in Chinese, please differentiate Traditional Chinese and Simplified Chinese. 
@@ -29,29 +28,38 @@ export class AdvisoryFeedbackPromptChainingService {
     return languagePrompt.pipe(this.model).pipe(new StringOutputParser());
   }
 
+  private createTopicChain() {
+    const topicTemplate = 'What is the topic of this feedback? Feedback: {feedback}';
+    const topicPrompt = PromptTemplate.fromTemplate<{ feedback: string }>(topicTemplate);
+
+    return topicPrompt.pipe(this.model).pipe(new StringOutputParser());
+  }
+
   private createSentimentChain() {
-    const sentimentTemplate = `What are the sentiment and topic of this feedback? Please give result in JSON { "sentiment": string, "topic": string }.
+    const sentimentTemplate = `What is the sentiment of this feedback?
     When the sentiment is positive, return 'POSITIVE', is neutral, return 'NEUTRAL', is negative, return 'NEGATIVE'. 
     Feedback: {feedback}`;
     const sentimentPrompt = PromptTemplate.fromTemplate<{ feedback: string }>(sentimentTemplate);
 
-    return sentimentPrompt.pipe(this.model).pipe(new JsonOutputParser());
+    return sentimentPrompt.pipe(this.model).pipe(new StringOutputParser());
   }
 
   async generateFeedback(prompt: string): Promise<string> {
     try {
       const languageChain = this.createFindLanguageChain();
       const sentimentChain = this.createSentimentChain();
+      const topicChain = this.createTopicChain();
 
       const feedbackPrompt = PromptTemplate.fromTemplate(`
         The customer wrote a {sentiment} feedback about {topic} in {language}. Feedback: {feedback}
         Please give a short reply in the same language. 
       `);
 
-      const combinedChain = RunnableSequence.from<RunnableFeedbackInput>([
+      const combinedChain = RunnableSequence.from([
         {
           language: languageChain,
-          analysis: sentimentChain,
+          sentiment: sentimentChain,
+          topic: topicChain,
           feedback: (input) => input.feedback,
         },
         feedbackPrompt,
