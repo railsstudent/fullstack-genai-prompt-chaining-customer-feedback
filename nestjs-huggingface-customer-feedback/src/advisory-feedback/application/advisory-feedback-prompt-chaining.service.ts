2726d782db9f035b05413c39030abc4560c8e005
Connie Leung <cleung334@gmail.com>
2024-05-30 23:45:44 +0800

    feat: formulate huggingface chatComplete

--- nestjs-huggingface-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
+++ nestjs-huggingface-customer-feedback/src/advisory-feedback/application/advisory-feedback-prompt-chaining.service.ts
@@ -1,78 +1,78 @@
 import { HfInference } from '@huggingface/inference';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { HUGGINGFACE_CHAT_PARAMETERS, HUGGINGFACE_INFERENCE } from './constants/huggingface.constant';
 import { env } from '~configs/env.config';
-import { ChatComputInputParameters } from './providers/huggingface-chat-parameters.provider';
+import { HUGGINGFACE_INFERENCE } from './constants/huggingface.constant';
 
 @Injectable()
 export class AdvisoryFeedbackPromptChainingService {
   private readonly logger = new Logger(AdvisoryFeedbackPromptChainingService.name);
 
-  constructor(
-    @Inject(HUGGINGFACE_INFERENCE) private hfInference: HfInference,
-    @Inject(HUGGINGFACE_CHAT_PARAMETERS) private chatParameters: ChatComputInputParameters,
-  ) {}
-
-  // // three chains:
-  // // chain 1: find the language of the feedback
-  // // chain 2: identity the sentiment of the feedback
-  // // chain 3: identity the topic of the feedback
-  // // chain 4: combine chain 1, 2 and 3 into a new chain. Then, address the topic and reply in the same language
-  // private createFindLanguageChain() {
-  //   const languageTemplate = `What is the language of this feedback?
-  //   If the feedback is written in Chinese, please differentiate Traditional Chinese and Simplified Chinese.
-  //   Please give me the language name, and nothing else. Delete the trailing newline character
-  //   Feedback: {feedback}`;
-  //   const languagePrompt = PromptTemplate.fromTemplate<CustomerFeedback>(languageTemplate);
-
-  //   return languagePrompt.pipe(this.model).pipe(new StringOutputParser());
-  // }
-
-  // private createTopicChain() {
-  //   const topicTemplate = `What is the topic of this feedback?
-  //   Just the topic and explanation is not needed. Delete the trailing newline character
-  //   Feedback: {feedback}`;
-  //   const topicPrompt = PromptTemplate.fromTemplate<CustomerFeedback>(topicTemplate);
-
-  //   return topicPrompt.pipe(this.model).pipe(new StringOutputParser());
-  // }
-
-  // private createSentimentChain() {
-  //   const sentimentTemplate = `What is the sentiment of this feedback? No explaination is needed.
-  //   When the sentiment is positive, return 'POSITIVE', is neutral, return 'NEUTRAL', is negative, return 'NEGATIVE'.
-  //   Feedback: {feedback}`;
-  //   const sentimentPrompt = PromptTemplate.fromTemplate<CustomerFeedback>(sentimentTemplate);
-
-  //   return sentimentPrompt.pipe(this.model).pipe(new StringOutputParser());
-  // }
+  constructor(@Inject(HUGGINGFACE_INFERENCE) private hfInference: HfInference) {}
 
   async generateFeedback(feedback: string): Promise<string> {
     try {
-      this.hfInference.chatCompletion({
+      const chatLanguage = await this.hfInference.chatCompletion({
+        accessToken: env.HUGGINGFACE.API_KEY,
         model: env.HUGGINGFACE.MODEL_NAME,
-        messages: [],
-        ...this.chatParameters,
+        temperature: 0.1,
+        top_p: 0.5,
+        max_tokens: 2048,
+        messages: [
+          {
+            role: 'user',
+            content: `What is the language used to write the feedback? Give me the language name, no explanation, no formal response.
+            When the feedback is written in Traditional Chinese, return Traditional Chinese. When the feedback is written in 
+            Simplified Chinese, return Simplified Chinese.
+            `,
+          },
+          {
+            role: 'assistant',
+            content: 'What is the feedback?',
+          },
+          {
+            role: 'user',
+            content: feedback,
+          },
+        ],
       });
-      // const chainMap = RunnableMap.from<CustomerFeedback>({
-      //   language: this.createFindLanguageChain(),
-      //   sentiment: this.createSentimentChain(),
-      //   topic: this.createTopicChain(),
-      //   feedback: ({ feedback }) => feedback,
-      // });
 
-      // const feedbackPrompt =
-      //   PromptTemplate.fromTemplate(`The customer wrote a {sentiment} feedback about {topic} in {language}. Feedback: {feedback}
-      //   Please give a short reply in the same language.`);
+      const language = (chatLanguage.choices[0].message.content || '').trim();
+      console.log(language);
 
-      // const combinedChain = RunnableSequence.from([chainMap, feedbackPrompt, this.model, new StringOutputParser()]);
+      const chatSentiment = await this.hfInference.chatCompletion({
+        accessToken: env.HUGGINGFACE.API_KEY,
+        model: env.HUGGINGFACE.MODEL_NAME,
+        temperature: 0.1,
+        top_p: 0.5,
+        max_tokens: 2048,
+        messages: [
+          {
+            role: 'user',
+            content: `Identify the sentiment and topic of feedback and return the JSON output { "sentiment": string, "topic": string }.
+            When the sentiment is positive, return 'POSITIVE', is neutral, return 'NEUTRAL', is negative, return 'NEGATIVE'.
+            Keep the sub-topics 3 or less.
+            Do not provide explanation for either fields.
+            `,
+          },
+          {
+            role: 'assistant',
+            content: 'What is the feedback?',
+          },
+          {
+            role: 'user',
+            content: feedback,
+          },
+        ],
+      });
 
-      // const response = await combinedChain.invoke({
-      //   feedback,
-      // });
+      try {
+        const strSentiment = (chatSentiment.choices[0].message.content || '{}').trim();
+        const jsonData = JSON.parse(strSentiment);
+        console.log(jsonData);
+      } catch {
+        console.log({});
+      }
 
-      // this.logger.log(response);
-
-      // return response;
       return Promise.resolve(feedback);
     } catch (ex) {
       console.error(ex);
--- nestjs-huggingface-customer-feedback/src/advisory-feedback/application/providers/huggingface-chat-parameters.provider.ts
+++ nestjs-huggingface-customer-feedback/src/advisory-feedback/application/providers/huggingface-chat-parameters.provider.ts
@@ -3,17 +3,19 @@ import { HUGGINGFACE_CHAT_PARAMETERS } from '../constants/huggingface.constant';
 
 export type ChatComputInputParameters = {
   temperature: number;
-  top_k: number;
+  // top_k: number;
   top_p: number;
   max_tokens: number;
+  // max_new_tokens: number;
 };
 
 export const HuggingFaceChatParametersProvider: Provider<ChatComputInputParameters> = {
   provide: HUGGINGFACE_CHAT_PARAMETERS,
   useValue: {
-    temperature: 0,
-    top_k: 10,
+    temperature: 0.1,
+    // top_k: 10,
     top_p: 0.5,
     max_tokens: 2048,
+    // max_new_tokens: 2048,
   },
 };
