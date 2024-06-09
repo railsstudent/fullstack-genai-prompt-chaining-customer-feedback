fc2c176755033583fc2dd660c8e3bf0b25a949b9
Connie Leung <cleung334@gmail.com>
2024-06-09 22:55:34 +0800

    fixup! refactor: revise Swagger description

--- nestjs-langchain-customer-feedback/src/core/bootstrap.ts
+++ nestjs-langchain-customer-feedback/src/core/bootstrap.ts
@@ -37,7 +37,7 @@ export class Bootstrap {
   setupSwagger() {
     const config = new DocumentBuilder()
       .setTitle('ESG Advisory Reply with Langchain multiple chains and Gemini')
-      .setDescription('Integrate with Gemini to improve ESG advisory feebacking by prompt chaining')
+      .setDescription('Integrate with Langchain to improve ESG advisory feebacking by prompt chaining')
       .setVersion('1.0')
       .addTag('Langchain, Gemini 1.5 Pro Model, Multiple Chains')
       .build();
